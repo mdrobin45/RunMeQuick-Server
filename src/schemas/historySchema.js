cce1bbd64af98707ab9d5bfd31aaa7a6057679bf
Md Robin Rana <developer.smrobin45@gmail.com>
2024-01-06 10:26:36 +0600

    edit history schema

--- src/schemas/historySchema.js
+++ src/schemas/historySchema.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const historySchema = mongoose.Schema({
    label: { type: String, required: true },
+   value: { type: String, required: true },
    email: { type: String, required: true },
    sourceCode: { type: String, required: true },
    output: { type: String, required: true },
